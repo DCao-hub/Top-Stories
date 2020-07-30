58bc1e042c5fc90d6dc00ec431ba197b98bb03cf
diana <dianacao777@gmail.com>
2020-07-30 11:58:14 -0400

    Initial Commit

---
+++ Top StoriesTests/Top_StoriesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Top_StoriesTests.swift
+//  Top StoriesTests
+//
+//  Created by Diana Cao on 7/30/20.
+//  Copyright © 2020 Diana Cao. All rights reserved.
+//
+
+import XCTest
+@testable import Top_Stories
+
+class Top_StoriesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
